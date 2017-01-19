64b92579aaaf9ad3e4c6be08b9aacab66272124a
root <root@localhost.localdomain>
2017-01-18 19:58:56 -0800

    add cost and eplise

--- bp-nn/bp_nn_howard.py
+++ bp-nn/bp_nn_howard.py
@@ -13,7 +13,6 @@ def sigmoid(z):
 def sigmoid_derivative(z):
     return np.multiply(sigmoid(z),(1 - sigmoid(z)))
 
-
 def plot_cost(cost):
     plt.xlabel('iteration number')
     plt.ylabel('cost value')
@@ -23,54 +22,71 @@ def plot_cost(cost):
     plt.plot(leng, cost)
     plt.show()
 
-alpha = 0.1
-reg_lambda = 0.3
+class ann_bp(object):
+    def __init__(self,data_array,num_2,num_3,alpha,reg_lambda,eplise,iter_max):
+        np.random.seed(1)
+        self.nrow,self.ncol = data_array.shape
+        self.scale_X = preprocessing.scale(data_array[:,0:self.ncol-1])
+        self.W1 = np.random.random(size=(self.ncol-1,num_2)) 
+        self.b1 = np.random.random(size=(1,num_2))
+        self.W2 = np.random.random(size=(num_2,num_3))
+        self.b2 = np.random.random(size=(1,num_3))
+        self.X = np.hstack((self.scale_X,np.repeat(1,self.nrow).reshape(self.nrow,1)))
+        self.Y = data_array[:,self.ncol-1].reshape(self.nrow,1)
+        self.alpha = alpha
+        self.reg_lambda = reg_lambda
+        self.eplise = eplise
+        self.iter_max = iter_max
+    def get_arg(self):
+        self.costJs =[]
+        iter = 0
+        while iter < self.iter_max:
+            
+            W_b_1 = np.vstack((self.W1,self.b1))
+            z2 = np.dot(self.X,W_b_1)
+            a2 = sigmoid(z2)
+            a2_1 = np.hstack((a2,np.repeat(1,self.nrow).reshape(self.nrow,1)))
+            
+            W_b_2 = np.vstack((self.W2,self.b2))
+            z3 = np.dot(a2_1,W_b_2)
+            a3 = sigmoid(z3)
+            self.a3 = a3
+            J = np.sum((self.Y-a3)**2)
+            self.costJs.append(J)
+            
+            delta_3 = -np.multiply((self.Y - a3),sigmoid_derivative(z3))
+            delta_2 = np.multiply(np.dot(delta_3,self.W2.T) ,sigmoid_derivative(z2))
+            
+            Delta_W_2_J = np.dot(a2.T, delta_3) 
+            Delta_W_1_J = np.dot(self.scale_X.T,delta_2)
+            Delta_b_2_J = delta_3.sum(axis = 0)
+            Delta_b_1_J = delta_2.sum(axis = 0)
+        
+            Delta_W_2_J += self.reg_lambda * self.W2
+            Delta_W_1_J += self.reg_lambda * self.W1
+        
+            self.W2 += -self.alpha * Delta_W_2_J
+            self.W1 += -self.alpha * Delta_W_1_J
+            self.b1 += -self.alpha * Delta_b_1_J
+            self.b2 += -self.alpha * Delta_b_2_J
+            ep = np.sum(np.fabs(Delta_W_2_J)) + np.sum(np.fabs(Delta_W_1_J)) \
+                +np.sum(np.fabs(Delta_b_2_J)) + np.sum(np.fabs(Delta_b_1_J))
+            if ep < self.eplise:
+               return self
+        return self
 
-data = pd.read_csv('trainData.csv')
-data_array = np.array(data)
-nrow,ncol = data_array.shape
-scale_X = preprocessing.scale(data_array[:,0:ncol-1])
-X = np.hstack((scale_X,np.repeat(1,nrow).reshape(nrow,1)))
-Y = data_array[:,ncol-1].reshape(nrow,1)
-
-np.random.seed(1)
-W1 = np.random.random(size=(4,3))
-b1 = np.random.random(size=(1,3))
-W2 = np.random.random(size=(3,1))
-b2 = np.random.random(size=(1,1))
-
-costJs =[]
-
-for i in xrange(1000):
-
-    W_b_1 = np.vstack((W1,b1))
-    z2 = np.dot(X,W_b_1)
-    a2 = sigmoid(z2)
-    a2_1 = np.hstack((a2,np.repeat(1,nrow).reshape(nrow,1)))
+if __name__ == '__main__':
     
-    W_b_2 = np.vstack((W2,b2))
-    z3 = np.dot(a2_1,W_b_2)
-    a3 = sigmoid(z3)
-    J = np.sum((Y-a3)**2)
-    costJs.append(J)
-    
-    delta_3 = -np.multiply((Y - a3),sigmoid_derivative(z3))
-    delta_2 = np.multiply(np.dot(delta_3,W2.T) ,sigmoid_derivative(z2))
-    
-    Delta_W_2_J = np.dot(a2.T, delta_3) 
-    Delta_W_1_J = np.dot(scale_X.T,delta_2)
-    Delta_b_2_J = delta_3.sum(axis = 0)
-    Delta_b_1_J = delta_2.sum(axis = 0)
-
-    Delta_W_2_J += reg_lambda * W2
-    Delta_W_1_J += reg_lambda * W1
-
-    W2 += -alpha * Delta_W_2_J
-    W1 += -alpha * Delta_W_1_J
-    b1 += -alpha * Delta_b_1_J
-    b2 += -alpha * Delta_b_2_J
-
-plot_cost(costJs)
-
+    alpha = 0.1
+    reg_lambda = 0.3
+    data = pd.read_csv('trainData.csv')
+    data_array = np.array(data)
+    num_2 = 3
+    num_3 = 2
+    iter_max = 1000
+    eplise = 0.01
+    ann_o = ann_bp(data_array,num_2,num_3,alpha,reg_lambda,eplise,iter_max)
+    ann_o.get_arg()
+    plot_cost(ann_o.costJs)
 
 
--- softmax/softmax_howrad.py
+++ softmax/softmax_howrad.py
@@ -3,7 +3,7 @@
 import numpy as np
 from sklearn import preprocessing
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def get_p(j,x,theta):
     kk,_ = theta.shape
@@ -20,74 +20,102 @@ def get_01(y,j):
        return 0
 
 
+def plot_cost(cost):
+    plt.xlabel('iteration number')
+    plt.ylabel('cost value')
+    plt.title('curve of cost value')
+    klen = len(cost)
+    leng = np.linspace(1, klen, klen)
+    plt.plot(leng, cost)
+    plt.show()  
+
 class soft_max(object):
-    def __init__(self,X,Y,Y_mat,theta,alpha,lam,iter_max,k):
-        self.X = X
-        self.Y = Y
-        self.Y_mat = Y_mat
-        self.theta = theta
+    def __init__(self,data_arry,alpha,lam,iter_max,k,eplise):
+        self.nrow,self.ncol = data_array.shape
+        self.scale_X = preprocessing.scale(data_array[:,0:self.ncol-1])
+        self.X = np.hstack((self.scale_X,np.repeat(1,self.nrow).reshape(self.nrow,1)))
+        self.Y = data_array[:,self.ncol-1].reshape(self.nrow,1) - 1
+        self.k = k
+        self.Y_mat = np.zeros((self.nrow,3))
+        for i in xrange(self.nrow):
+            self.Y_mat[i,self.Y[i][0]] = 1.0
+
+        np.random.seed(1)
+        self.theta = np.random.random((self.k,self.ncol))/self.nrow
         self.alpha = alpha
         self.lam = lam
         self.iter_max = iter_max
-        self.k = k
-        self.nrow,self.ncol = X.shape
+        self.eplise = eplise
+
     def softmax_train_method1(self):
-        nrow,ncoll = self.X.shape
-        for _ in xrange(self.iter_max):
+        self.costs = []
+        iter = 0
+        while iter < self.iter_max:
+            cost_first = 0
+            cost_second = 0
+            ep = 0
             for j in xrange(self.k):
                 Delta_j = np.zeros((1,self.ncol))
-                for i in xrange(nrow):
+                for i in xrange(self.nrow):
                     x = self.X[i]
                     y = self.Y[i]
-                    Delta_j += x * (get_01(y,j) - get_p(j,x,self.theta))
+                    tf = get_01(y,j)
+                    p = get_p(j,x,self.theta)
+                    #gradient of softmax
+                    Delta_j += x * (tf - p)
+                    #loss of softmax
+                    cost_first += tf * np.log(p)
+                cost_second += np.sum(self.theta[j] ** 2)
+
                 # L2 regulation
                 Delta_j = - float(1)/self.nrow * Delta_j + self.lam * self.theta[j]
+                ep += np.sum(np.fabs(Delta_j))
+                # update theta
                 self.theta[j] = self.theta[j] -  self.alpha * Delta_j
+            cost = -cost_first/float(self.nrow) +  self.lam * cost_second / 2
+            self.costs.append(cost )
+            iter += 1
+            if ep < self.eplise:
+                return self 
         return self
 
     def softmax_train_method2(self):
-        for _ in xrange(self.iter_max):
+        self.costs = []
+        iter = 0
+        while( iter < self.iter_max):
             exp_theta_x = np.exp(np.dot(self.X,self.theta.T))
             exp_theta_x_p = exp_theta_x / exp_theta_x.sum(axis =1).reshape(self.nrow,1)
             Delta_theta = -np.dot((self.Y_mat - exp_theta_x_p).T,self.X)/float(self.nrow)\
                          + self.lam * self.theta
+            cost = -np.sum(self.Y_mat * np.log(exp_theta_x_p))/self.nrow \
+                   + self.lam / 2 * np.sum(self.theta ** 2)
+            cost2 = -np.sum( np.log(exp_theta_x_p[range(self.nrow),self.Y.astype(np.int).reshape((self.nrow,))]))/self.nrow \
+                   + self.lam / 2 * np.sum(self.theta ** 2)
+
             self.theta -= self.alpha * Delta_theta
+          
+            self.costs.append(cost)
+            iter += 1
+            if np.sum(np.fabs(Delta_theta)) < self.eplise:
+                return self
         return self
 
 if __name__ == '__main__':
-
     data = pd.read_csv('iris_softmax.csv')
-
     #data = pd.read_csv('http://oheum0xlq.bkt.clouddn.com/iris_softmax.csv')
-
     data_array = np.array(data)
-
-    nrow,ncol = data_array.shape
-
-    scale_X = preprocessing.scale(data_array[:,0:ncol-1])
-
-    X = np.hstack((scale_X,np.repeat(1,nrow).reshape(nrow,1)))
-
-    Y = data_array[:,ncol-1].reshape(nrow,1) - 1
-
     k = 3
-
-    Y_mat = np.zeros((nrow,3))
-    for i in xrange(nrow):
-        Y_mat[i,Y[i][0]] = 1.0
-
-    np.random.seed(1)
-    theta = np.random.random((k,ncol))/nrow
     alpha = 0.5 
     lam = 0.1
-    iter_max = 10
-    soft = soft_max(X,Y,Y_mat,theta,alpha,lam,iter_max,k)
-    a = soft.softmax_train_method1()
+    iter_max = 100
+    eplise = 0.01
+    soft = soft_max(data_array,alpha,lam,iter_max,k,eplise)
+    #a = soft.softmax_train_method1()
     b = soft.softmax_train_method2()
-    p_y = np.dot(X,a.theta.T)
-    np.argmax(p_y,axis=1)
-
-
+    #p_y = np.dot(a.X,a.theta.T)
+    p_y = np.dot(b.X,b.theta.T)
+    print np.argmax(p_y,axis=1)
+    plot_cost(b.costs)
 
 
 
