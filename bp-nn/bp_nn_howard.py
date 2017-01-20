f5beb32e46a12cb5a645509dd69805e39bf37c55
root <root@localhost.localdomain>
2017-01-19 19:11:22 -0800

    in output layer add softmax  with  ann

--- bp-nn/bp_nn_howard.py
+++ bp-nn/bp_nn_howard.py
@@ -37,7 +37,7 @@ class ann_bp(object):
         self.reg_lambda = reg_lambda
         self.eplise = eplise
         self.iter_max = iter_max
-    def get_arg(self):
+    def get_arg_sigmoid(self):
         self.costJs =[]
         iter = 0
         while iter < self.iter_max:
@@ -75,6 +75,47 @@ class ann_bp(object):
                return self
         return self
 
+    def get_arg_softmax(self):
+        self.costJs =[]
+        iter = 0
+        while iter < self.iter_max:
+            
+            W_b_1 = np.vstack((self.W1,self.b1))
+            z2 = np.dot(self.X,W_b_1)
+            a2 = sigmoid(z2)
+            a2_1 = np.hstack((a2,np.repeat(1,self.nrow).reshape(self.nrow,1)))
+            
+            W_b_2 = np.vstack((self.W2,self.b2))
+            z3 = np.dot(a2_1,W_b_2)
+            a3 = np.exp(z3)/np.exp(z3).sum(axis=1).reshape(self.nrow,1)
+            self.a3 = a3
+            idx_col = self.Y.astype(np.int).reshape((self.nrow,))
+            idx_row = range(self.nrow)
+            J = -np.sum(np.log(a3[idx_row,idx_col]))/float(self.nrow)
+            self.costJs.append(J)
+            delta_3 = a3
+            delta_3[idx_row,idx_col] -= 1
+            delta_2 = np.multiply(np.dot(delta_3,self.W2.T) ,sigmoid_derivative(z2))
+            
+            Delta_W_2_J = np.dot(a2.T, delta_3) 
+            Delta_W_1_J = np.dot(self.scale_X.T,delta_2)
+            Delta_b_2_J = delta_3.sum(axis = 0)
+            Delta_b_1_J = delta_2.sum(axis = 0)
+        
+            Delta_W_2_J += self.reg_lambda * self.W2
+            Delta_W_1_J += self.reg_lambda * self.W1
+        
+            self.W2 += -self.alpha * Delta_W_2_J
+            self.W1 += -self.alpha * Delta_W_1_J
+            self.b1 += -self.alpha * Delta_b_1_J
+            self.b2 += -self.alpha * Delta_b_2_J
+            ep = np.sum(np.fabs(Delta_W_2_J)) + np.sum(np.fabs(Delta_W_1_J)) \
+                +np.sum(np.fabs(Delta_b_2_J)) + np.sum(np.fabs(Delta_b_1_J))
+            if ep < self.eplise:
+               return self
+        return self
+
+
 if __name__ == '__main__':
     
     alpha = 0.1
@@ -83,10 +124,11 @@ if __name__ == '__main__':
     data_array = np.array(data)
     num_2 = 3
     num_3 = 2
-    iter_max = 1000
-    eplise = 0.01
+    iter_max = 10
+    eplise = 0.1
     ann_o = ann_bp(data_array,num_2,num_3,alpha,reg_lambda,eplise,iter_max)
-    ann_o.get_arg()
+    # if choose sigmoid please let num_3 = 1, softmax let num_3 =2
+    ann_o.get_arg_softmax()
     plot_cost(ann_o.costJs)
-
+    print ann_o.a3.argmax(axis=1)
 
